2aa917d22defd5ef083cfdbd3475f159e273f6f5
Vivekgunjal <vivekgunjal619@gmail.com>
2023-05-09 15:04:37 +0530

    tokens 300

--- pages/api/ChatGPTCodeConverter.js
+++ pages/api/ChatGPTCodeConverter.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
       model: "text-davinci-003",
       prompt: prompt,
       temperature: 0,
-      max_tokens: 500,
+      max_tokens: 300,
       top_p: 1.0,
       frequency_penalty: 0.0,
       presence_penalty: 0.0,
